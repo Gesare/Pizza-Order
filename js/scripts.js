97cc25a94f56f141ebdb1643246a7170ae54fc59
odetteahishakiye <ahiode6@gmail.com>
2019-08-04 20:47:14 +0200

    Create objects from constructors

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 // Business logic
-function Size(name,price){
-    this.name=pizzaSize;
-    this.price=price;
+function Size(name, price) {
+    this.name = pizzaSize;
+    this.price = price;
 }
-function Crust(name,price){
-    this.name=crustName;
-    this.price=price;
+function Crust(name, price) {
+    this.name = crustName;
+    this.price = price;
 }
-function Toppings(name,price){
-    this.name=toppingName;
-    this.price=price;
+function Toppings(name, price) {
+    this.name = toppingName;
+    this.price = price;
 }
 
 function Order(size, crust, topping, quantity) {
@@ -25,6 +25,13 @@ function Order(size, crust, topping, quantity) {
 
 // UI Logic
 $(document).ready(function () {
+    var small = new size("Small", 1500);
+    var medium = new size("Medium", 2500);
+    var large = new size("Large", 4500);
+
+    var crispy = new crust("Crispy", 500);
+    var stuffed = new crust("Stuffed", 700);
+    var gluten = new crust("Gluten", 1000);
     $("#pick").click(function () {
         $("#price").show();
         $("#order").show();
