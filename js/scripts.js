d0c1e5a07fe9909dc9c58c865e52220539641222
odetteahishakiye <ahiode6@gmail.com>
2019-08-05 12:14:05 +0200

    Create a function

--- js/scripts.js
+++ js/scripts.js
@@ -25,25 +25,77 @@ $(document).ready(function () {
     $('form#ordering').submit(function (event) {
         event.preventDefault();
         var newOrder;
-        if($('small').val()){
-            var choosenSize=$('#small').val()+""+"price=1800 Rwf";
-            console.log(choosenSize);
+        var sizedPrice;
+        if ($("input[name='size']:checked").val()==="Small") {
+            var choosenSize = $("input[name='size']:checked").val();
+            sizedPrice=1800;
         }
-        else if()
+        else if($("input[name='size']:checked").val()==="Medium"){
+            var choosenSize=$("input[name='size']:checked").val();
+            sizedPrice=2500;
+        }
+        else{
+            var choosenSize=$("input[name='size']:checked").val();
+            sizedPrice=4000;
+        }
+        var crustPrice;
+        if (($("input[name='crust']:checked").val()==="Crispy")||($("input[name='crust']:checked").val()==="Stuffed")) {
+            var choosenCrust = $("input[name='crust']:checked").val();
+            crustPrice=500;
+        }
+        else{
+            var choosenCrust=$("input[name='crust']:checked").val();
+            crustPrice=700;
+        }
+        var choosenToppings = [];
+        $("#toppings option:selected").text().forEach(function () {
+            choosenToppings.push($(this).val());
+            var specialTopping = 0;
+            function topping(choosenToppings) {
+                for (var top = 0; top < choosenToppings.length; top++) {
+                    if (choosenToppings[top] === "Mushrooms") {
+                        specialTopping += 600;
+                    }
+                    else if ((choosenToppings[top] === "Onions")||(choosenToppings[top]==="Green peppers")) {
+                        specialTopping += 300;
+                    }
+                    else if (choosenToppings[top] === "Pepperoni") {
+                        specialTopping += 700;
+                    }
+                    else if(choosenToppings==="Bacon and shrimp"){
+                        specialTopping+=1000;
+                    }
+                    else if((choosenToppings==="Barbecue sauce and chicken")||(choosenToppings==="Taco toppings")){
+                        specialTopping+=1200;
+                    }
+                    else{
+                        specialTopping+=500;
+                    }
+                    
+                }
+                return specialTopping;
+            }
+        });
     });
-    // $("#pick").click(function () {
-    //     $("#price").show();
-    //     // if ()
-    //     $("#order").show();
-    //     $("#check").show();
-    //     $("#check").click(function () {
-    //         $("#price").show();
-    //         $("#order").show();
-    //         // $("#delivery").show();
-    //         // $("#charges").show();
-    //         $("#add").show();
-    //     });
-    // });
+    var newOrder = new Order(choosenSize,choosenCrust,specialTopping,number, deliverance);
+    console.log(newOrder);
+    console.log(specialTopping);
+    
+    $("#pick").click(function () {
+        $("#price").show();
+        $("#pizza-amount").text()=sizedPrice+crustPrice+specialTopping+" Rwf";
+        $("#order").show();
+        $('ul#details').append("<li><span>" +newOrder.aboutOrder() +"</span></li>");
+        // $("#details").val()=(choosenSize+", "+choosenCrust+", "+choosenToppings)*number;
+        $("#check").show();
+        $("#check").click(function () {
+            $("#price").show();
+            $("#order").show();
+            // $("#delivery").show();
+            // $("#charges").show();
+            $("#add").show();
+        });
+    });
     // $("#deliver").click(function () {
     //     alert("Please, mention your location!");
     //     var location = prompt("Enter your location address!");
