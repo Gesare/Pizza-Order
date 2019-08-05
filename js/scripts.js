35434e99dfcdb58ce542cd24d97c09c9307e7632
odetteahishakiye <ahiode6@gmail.com>
2019-08-05 21:43:49 +0200

    Delete useless comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,68 +1,8 @@
-// Business logic
-// function Order(size, crust, topping, number) {
-//     this.pizzaSize = size;
-//     this.pizzaCrust = crust;
-//     this.pizzaToppings = topping;
-//     this.pizzaNumber = number;
-// }
-// Order.prototype.aboutOrder = function () {
-//     return this.pizzaSize + "," + this.pizzaCrust + "," + this.pizzaToppings + "," + this.pizzaNumber;
-// }
 
-
-var choosenNumber = parseInt($("#number").val());
 // User Interface logic
 $(document).ready(function () {
     $('form#ordering').submit(function (event) {
         event.preventDefault();
-        // var sizedPrice;
-        // var choosenSize = $("input[name='size']:checked").val();
-        // if (choosenSize === "Small") {
-        //     sizedPrice = 1800;
-        // }
-        // else if (choosenSize === "Medium") {
-        //     sizedPrice = 3000;
-        // }
-        // else {
-        //     sizedPrice = 5000;
-        // }
-        // var crustPrice;
-        // var choosenCrust = $("input[name='crust']:checked").val();
-
-        // if ((choosenCrust === "Crispy") || (choosenCrust === "Stuffed")) {
-        //     crustPrice = 500;
-        // }
-        // else {
-        //     crustPrice = 700;
-        // }
-        // var choosenToppings = $("#toppings option:selected").val();
-        // $("#toppings option:selected").each(function () {
-        //     choosenToppings.push($(this).val());
-        // });
-        // var addToppings = 0;
-        // for (var top = 0; top < choosenToppings.length; top++) {
-        //     if (choosenToppings[top] === "Mushrooms") {
-        //         addToppings += 600;
-        //     }
-        //     else if ((choosenToppings[top] === "Onions") || (choosenToppings[top] === "Green peppers")) {
-        //         addToppings += 300;
-        //     }
-        //     else if (choosenToppings[top] === "Pepperoni") {
-        //         addToppings += 700;
-        //     }
-        //     else if (choosenToppings === "Bacon and shrimp") {
-        //         addToppings += 1000;
-        //     }
-        //     else if ((choosenToppings === "Barbecue sauce and chicken") || (choosenToppings === "Taco toppings")) {
-        //         addToppings += 1200;
-        //     }
-        //     else {
-        //         addToppings += 500;
-        //     }
-        // }
-
-
-
     });
 
     $("#pick").click(function () {
