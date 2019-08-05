edca15def51cfdb541745a444e92333e7583007c
odetteahishakiye <ahiode6@gmail.com>
2019-08-05 20:32:36 +0200

    Output the total cost

--- js/scripts.js
+++ js/scripts.js
@@ -61,7 +61,7 @@ $(document).ready(function () {
         //     }
         // }
 
-        
+
 
     });
 
@@ -89,7 +89,7 @@ $(document).ready(function () {
         else {
             crustPrice = 700;
         }
-        var addToppings=0;
+        var addToppings = 0;
         var choosenToppings = $("#toppings option:selected").val();
         if (choosenToppings === "Mushrooms") {
             addToppings += 600;
@@ -112,30 +112,85 @@ $(document).ready(function () {
         $("#check").click(function () {
             $("#price").show();
             $("#order").show();
-        var pizzaNumber = parseInt($("#number").val());
-        var price = (sizedPrice + crustPrice + addToppings) * pizzaNumber;
-        $("#pizza-amount").text(price);
-        
-        var display = $("input[name='size']:checked").val() + ", " + $("input[name='crust']:checked").val() + ", " + $("#toppings option:selected").val();
-        $("#details").text(display);
-        // $("#details").val() = choosenSize + ", " + choosenCrust;
-        // $("#details").show();
-        
-        
-            $("#add").show();
+            var pizzaNumber = parseInt($("#number").val());
+            var price = (sizedPrice + crustPrice + addToppings) * pizzaNumber + "Rwf";
+            $("#pizza-amount").text(price);
+
+            var display = $("#number").val() + " " + $("input[name='size']:checked").val() + ", " + $("input[name='crust']:checked").val() + ", " + $("#toppings option:selected").val();
+            $("#details").text(display);
+
+            // $("#add").show();
+            // $(this).click(function(){
+            //     $("input[name='size']:checked").val()="";
+            //     $("input[name='crust']:checked").val()="";
+
+            // })
         });
     });
     $("#deliver").click(function () {
         alert("Please, mention your location!");
         var location = prompt("Enter your location address!");
         alert("Thanks, you will be delivered at " + location);
+
         $("#check").show();
+        var sizedPrice;
+        var choosenSize = $("input[name='size']:checked").val();
+        if (choosenSize === "Small") {
+            sizedPrice = 1800;
+        }
+        else if (choosenSize === "Medium") {
+            sizedPrice = 3000;
+        }
+        else {
+            sizedPrice = 5000;
+        }
+        var crustPrice;
+        var choosenCrust = $("input[name='crust']:checked").val();
+
+        if ((choosenCrust === "Crispy") || (choosenCrust === "Stuffed")) {
+            crustPrice = 500;
+        }
+        else {
+            crustPrice = 700;
+        }
+        var addToppings = 0;
+        var choosenToppings = $("#toppings option:selected").val();
+        if (choosenToppings === "Mushrooms") {
+            addToppings += 600;
+        }
+        else if ((choosenToppings === "Onions") || (choosenToppings === "Peppers")) {
+            addToppings += 300;
+        }
+        else if (choosenToppings === "Pepperoni") {
+            addToppings += 700;
+        }
+        else if (choosenToppings === "Bacon") {
+            addToppings += 1000;
+        }
+        else if ((choosenToppings === "Barbecue") || (choosenToppings === "Taco")) {
+            addToppings += 1200;
+        }
+        else {
+            addToppings += 500;
+        }
         $("#check").click(function () {
             $("#price").show();
             $("#order").show();
+            var pizzaNumber = parseInt($("#number").val());
+            var price = "Rwf " + (sizedPrice + crustPrice + addToppings) * pizzaNumber;
+            $("#pizza-amount").text(price);
+
+            var display = $("#number").val() + " " + $("input[name='size']:checked").val() + ", " + $("input[name='crust']:checked").val() + ", " + $("#toppings option:selected").val();
+            $("#details").text(display);
+
+            
+            // $("#add").show();
+            // $("#price").show();
+            // $("#order").show();
             $("#delivery").show();
             $("#charges").show();
-            $("#add").show();
-        });
+            $("#total").text() = "The total cost is: " + [2000 + (sizedPrice + crustPrice + addToppings) * pizzaNumber] + " Rwf!");
+        // $("#add").show();
     });
+});
 });
